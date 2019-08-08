17275e65233678057304a92118d83b7403d185cc
Oleg Kozlov <h1karo@outlook.com>
2019-08-09 01:55:09 +0300

    Updated copyright

--- src/MessageInterface.php
+++ src/MessageInterface.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * Relations Messenger Message Standard
+ *
+ * @link      https://gitlab.com/relmsg/message
+ * @copyright Copyright (c) 2018-2019 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/message
+ */
 
 namespace RM\API\Message;
 
--- src/MessageType.php
+++ src/MessageType.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * A Relations Messenger SDK
+ * Relations Messenger Message Standard
  *
- * @link      https://gitlab.com/relmsg/php-sdk
+ * @link      https://gitlab.com/relmsg/message
  * @copyright Copyright (c) 2018-2019 Relations Messenger
  * @author    h1karo <h1karo@outlook.com>
  * @license   Apache License 2.0
- * @license   https://legal.relmsg.ru/licenses/sdk
+ * @license   https://legal.relmsg.ru/licenses/message
  */
 
 namespace RM\API\Message;

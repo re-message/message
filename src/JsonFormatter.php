d47be224277584f28c2641f63fff82d47f080ac3
Oleg Kozlov <h1karo@outlook.com>
2020-01-18 17:01:18 +0300

    Update copyright

--- src/JsonFormatter.php
+++ src/JsonFormatter.php
@@ -1,12 +1,17 @@
 <?php
 /**
- * Relations Messenger API Message Standard
+ * This file is a part of Relations Messenger API Message Standard.
+ * This package is a part of Relations Messenger.
  *
  * @link      https://gitlab.com/relmsg/message
- * @copyright Copyright (c) 2018-2019 Relations Messenger
+ * @link      https://dev.relmsg.ru/packages/message
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
  * @author    h1karo <h1karo@outlook.com>
  * @license   Apache License 2.0
  * @license   https://legal.relmsg.ru/licenses/message
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace RM\Standards\Message;

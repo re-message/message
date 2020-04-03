fe5d2b04136522aa64fae4aa71c15d3595c941b6
Oleg Kozlov <h1karo@outlook.com>
2020-04-03 21:18:09 +0300

    Improve action message to use him in clients without the class creation
    Now bindable validatable action message will be in relmsg/message-api-bundle with the action registry

--- src/Action.php
+++ src/Action.php
@@ -18,32 +18,23 @@ namespace RM\Standard\Message;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use RM\Standard\Message\Exception\InvalidParameterException;
-use RM\Standard\Message\Exception\UnknownParameterException;
-use RM\Standard\Message\Exception\NonSerializableTypeException;
-use Symfony\Component\Validator\Constraint;
-use Symfony\Component\Validator\ConstraintViolationList;
-use Symfony\Component\Validator\ConstraintViolationListInterface;
-use Symfony\Component\Validator\Validation;
 
 /**
- * Abstract class for actions.
+ * Class Action
  *
  * @package RM\Standard\Message
  * @author  h1karo <h1karo@outlook.com>
  * @see     MessageType::ACTION
  */
-abstract class Action implements ValidatableMessageInterface
+class Action implements ActionInterface
 {
+    private string $name;
     private Collection $parameters;
 
-    /**
-     * Any data should NOT be passed with the constructor.
-     * Please use setters instead.
-     */
-    final public function __construct()
+    public function __construct(string $name, array $parameters = [])
     {
-        $this->parameters = new ArrayCollection();
+        $this->name = $name;
+        $this->parameters = new ArrayCollection($parameters);
     }
 
     /**
@@ -52,44 +43,9 @@ abstract class Action implements ValidatableMessageInterface
      * @return string
      * @see https://dev.relmsg.ru/api/actions
      */
-    abstract public static function getName(): string;
-
-    /**
-     * The constraints for all parameters.
-     * Format: parameter => constraints
-     *
-     * @return Constraint[][] Parameters constraints for this action.
-     */
-    abstract protected function getConstraints(): array;
-
-    /**
-     * The constraints for parameter.
-     *
-     * @param string|null $name The name of parameter.
-     *
-     * @return Constraint[]|null Parameter constraints or null if there are no constraints.
-     */
-    protected function getParameterConstraints(string $name): ?array
+    public function getName(): string
     {
-        if (!$this->hasParameter($name)) {
-            return null;
-        }
-
-        $constraints = $this->getConstraints();
-        return $constraints[$name];
-    }
-
-    /**
-     * Checks the existence of parameter by name.
-     * The parameter should be defined in {@see getConstraints()}.
-     *
-     * @param string $name
-     *
-     * @return bool
-     */
-    final public function hasParameter(string $name): bool
-    {
-        return array_key_exists($name, $this->getConstraints());
+        return $this->name;
     }
 
     /**
@@ -101,169 +57,39 @@ abstract class Action implements ValidatableMessageInterface
     }
 
     /**
-     * Bind given value to parameter and validate him.
-     *
-     * @param string $parameter
-     * @param mixed  $value
-     *
-     * @throws UnknownParameterException
-     * @throws NonSerializableTypeException
-     * @throws InvalidParameterException
-     */
-    final public function bind(string $parameter, $value): void
-    {
-        if (!$this->hasParameter($parameter)) {
-            throw new UnknownParameterException($parameter, static::getName());
-        }
-
-        if (is_object($value) || is_resource($value)) {
-            throw new NonSerializableTypeException($value);
-        }
-
-        $violations = $this->validateValue($parameter, $value);
-        if ($violations->count() !== 0) {
-            $violation = $violations->get(0);
-            throw new InvalidParameterException(static::getName(), $parameter, $value, $violation);
-        }
-
-        $this->parameters->set($parameter, $value);
-    }
-
-    /**
-     * Binds all parameters with validation.
-     *
-     * @param iterable $parameters
-     *
-     * @throws UnknownParameterException
-     * @throws NonSerializableTypeException
-     * @throws InvalidParameterException
-     */
-    final public function bindAll(iterable $parameters): void
-    {
-        foreach ($parameters as $parameter => $value) {
-            $this->bind($parameter, $value);
-        }
-    }
-
-    /**
-     * Returns the current value of the parameter or null if it does not exist.
-     *
-     * @param string $name
-     *
-     * @return mixed|null
-     */
-    final protected function getBoundValue(string $name)
-    {
-        return $this->parameters->get($name);
-    }
-
-    /**
-     * Checks the existence of value for the parameter.
+     * Checks the existence of parameter by name.
      *
      * @param string $name
      *
      * @return bool
      */
-    final protected function hasBoundValue(string $name): bool
+    public function hasParameter(string $name): bool
     {
         return $this->parameters->containsKey($name);
     }
 
     /**
-     * Returns the current parameter value or the default value or null.
+     * Returns the current parameter value or or null.
      *
      * @param string $name
      *
      * @return mixed|null
      */
-    final public function getValue(string $name)
+    public function getValue(string $name)
     {
-        return $this->getBoundValue($name) ?? $this->getDefaultValue($name);
+        return $this->parameters->get($name);
     }
 
     /**
-     * Checks the existence of value or default value for parameter.
+     * Checks the existence of value for parameter.
      *
      * @param string $name
      *
      * @return bool
      */
-    final public function hasValue(string $name): bool
+    public function hasValue(string $name): bool
     {
-        return $this->hasBoundValue($name) || $this->hasDefaultValue($name);
-    }
-
-    /**
-     * Returns a default values for action parameters.
-     * You can override this method to provide default values.
-     *
-     * @return array
-     */
-    public function getDefaultValues(): array
-    {
-        return [];
-    }
-
-    /**
-     * Returns the default value for parameter or null if it does not exist.
-     *
-     * @param string $name
-     *
-     * @return mixed|null
-     */
-    final public function getDefaultValue(string $name)
-    {
-        $defaults = $this->getDefaultValues();
-        return $defaults[$name] ?? null;
-    }
-
-    /**
-     * Checks the existence of default value for the parameter.
-     *
-     * @param string $name
-     *
-     * @return bool
-     */
-    final public function hasDefaultValue(string $name): bool
-    {
-        return array_key_exists($name, $this->getDefaultValues());
-    }
-
-    /**
-     * @inheritDoc
-     */
-    final public function validateAll(): ConstraintViolationListInterface
-    {
-        $violations = new ConstraintViolationList();
-
-        foreach (array_keys($this->getConstraints()) as $parameter) {
-            $v = $this->validateParameter($parameter);
-            $violations->addAll($v);
-        }
-
-        return $violations;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    final public function validateValue(string $parameter, $value): ConstraintViolationListInterface
-    {
-        $validator = Validation::createValidator();
-        $constraints = $this->getParameterConstraints($parameter);
-        return $validator->validate($value, $constraints);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    final public function validateParameter(string $parameter): ConstraintViolationListInterface
-    {
-        if ($this->hasDefaultValue($parameter) && !$this->hasBoundValue($parameter)) {
-            return new ConstraintViolationList();
-        }
-
-        return $this->validateValue($parameter, $this->getBoundValue($parameter));
+        return $this->parameters->containsKey($name);
     }
 
     /**
@@ -273,7 +99,7 @@ abstract class Action implements ValidatableMessageInterface
     {
         return [
             'type' => $this->getType(),
-            'name' => static::getName(),
+            'name' => $this->getName(),
             'parameters' => $this->parameters->toArray()
         ];
     }
---
+++ src/ActionInterface.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * This file is a part of Relations Messenger API Message Standard.
+ * This package is a part of Relations Messenger.
+ *
+ * @link      https://gitlab.com/relmsg/message
+ * @link      https://dev.relmsg.ru/packages/message
+ * @copyright Copyright (c) 2018-2020 Relations Messenger
+ * @author    h1karo <h1karo@outlook.com>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/message
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Message;
+
+/**
+ * Interface ActionInterface
+ *
+ * @package RM\Standard\Message
+ * @author  h1karo <h1karo@outlook.com>
+ * @see     MessageType::ACTION
+ */
+interface ActionInterface extends MessageInterface
+{
+    /**
+     * The unique name of action.
+     *
+     * @return string
+     * @see https://dev.relmsg.ru/api/actions
+     */
+    public function getName(): string;
+
+    /**
+     * Checks the existence of parameter by name.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasParameter(string $name): bool;
+
+    /**
+     * Returns the current parameter value or the default value or null.
+     *
+     * @param string $name
+     *
+     * @return mixed|null
+     */
+    public function getValue(string $name);
+
+    /**
+     * Checks the existence of value or default value for parameter.
+     *
+     * @param string $name
+     *
+     * @return bool
+     */
+    public function hasValue(string $name): bool;
+}

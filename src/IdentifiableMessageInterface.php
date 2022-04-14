0500d9e877b69d208d3e64c73cd4d5582d8289b4
Oleg Kozlov <h1karo@relmsg.ru>
2022-04-14 20:15:15 +0300

    Implement identifiable message contract

---
+++ src/IdentifiableMessageInterface.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is a part of Relations Messenger Message Standard.
+ * This package is a part of Relations Messenger.
+ *
+ * @see       https://github.com/relmsg/message
+ * @see       https://dev.relmsg.ru/packages/message
+ * @copyright Copyright (c) 2018-2022 Relations Messenger
+ * @author    Oleg Kozlov <h1karo@relmsg.ru>
+ * @license   Apache License 2.0
+ * @license   https://legal.relmsg.ru/licenses/message
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RM\Standard\Message;
+
+/**
+ * @author Oleg Kozlov <h1karo@relmsg.ru>
+ */
+interface IdentifiableMessageInterface extends MessageInterface
+{
+    /**
+     * Returns the unique identifier of message or null.
+     */
+    public function getId(): string|null;
+}

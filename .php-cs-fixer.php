97e3338513ccf06223b47bc04b2bef6bae1a7ec5
Oleg Kozlov <h1karo@remessage.ru>
2022-04-22 20:55:34 +0300

    Configure header rule for php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -6,6 +6,26 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
 ;
 
+$namespace = 'Re Message';
+$projectTitle = 'Message Standard';
+$projectName = 'message';
+$currentYear = date('Y');
+
+$header = <<<EOF
+    This file is a part of {$projectTitle}.
+    This package is a part of {$namespace}.
+
+    @link      https://github.com/re-message/{$projectName}
+    @link      https://dev.remessage.ru/packages/{$projectName}
+    @copyright Copyright (c) 2018-{$currentYear} {$namespace}
+    @author    Oleg Kozlov <h1karo@remessage.ru>
+    @license   Apache License 2.0
+    @license   https://legal.remessage.ru/licenses/{$projectName}
+
+    For the full copyright and license information, please view the LICENSE
+    file that was distributed with this source code.
+    EOF;
+
 $config = new PhpCsFixer\Config();
 return $config
     ->setRules(
@@ -28,6 +48,12 @@ return $config
             ],
             'phpdoc_tag_casing' => [
                 'tags' => ['inheritDoc'],
+            ],
+            'header_comment' => [
+                'header' => $header,
+                'comment_type' => 'comment',
+                'location' => 'after_open',
+                'separate' => 'bottom',
             ],
         ]
     )

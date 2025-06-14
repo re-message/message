12c65f9e39a06f277e281b6c12daeaedd93ab4a2
Oleg Kozlov <h1karo@remessage.ru>
2025-06-14 04:35:38 +0300

    Reconfigure file header in php cs fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -16,37 +16,22 @@
 
 use PhpCsFixer\Finder;
 use RM\Style\RuleSet\Config;
+use RM\Style\RuleSet\Header;
 
 $finder = Finder::create()
     ->in(__DIR__)
     ->append([__FILE__])
     ->exclude('vendor')
-    ->notPath('src/MessageType.php')
 ;
 
-$namespace = 'Re Message';
-$projectTitle = 'Message Standard';
-$projectName = 'message';
-$currentYear = date('Y');
+$header = new Header()
+    ->setNamespace('Re Message')
+    ->setProjectName('message')
+    ->setProjectTitle('Message Standard')
+    ->withAuthor('Oleg Kozlov', 'h1karo@outlook.com')
+;
 
-$header = <<<EOF
-    This file is a part of {$projectTitle}.
-    This package is a part of {$namespace}.
-
-    @link      https://github.com/re-message/{$projectName}
-    @link      https://dev.remessage.ru/packages/{$projectName}
-    @copyright Copyright (c) 2018-{$currentYear} {$namespace}
-    @author    Oleg Kozlov <h1karo@remessage.ru>
-    @license   Apache License 2.0
-    @license   https://legal.remessage.ru/licenses/{$projectName}
-
-    For the full copyright and license information, please view the LICENSE
-    file that was distributed with this source code.
-    EOF;
-
-$config = new Config();
-
-return $config
+return new Config()
     ->setHeader($header)
     ->setFinder($finder)
 ;

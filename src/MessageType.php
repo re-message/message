33aa656b3c9258f466b763a04aa237ab4a20067b
Oleg Kozlov <h1karo@remessage.ru>
2025-06-14 04:41:31 +0300

    Update copyright in file header

--- src/MessageType.php
+++ src/MessageType.php
@@ -1,17 +1,16 @@
 <?php
+
 /*
- * This file is a part of Message Standard.
- * This package is a part of Re Message.
+ * This file is part of Message Standard.
  *
- * @link      https://github.com/re-message/message
- * @link      https://dev.remessage.ru/packages/message
- * @copyright Copyright (c) 2018-2022 Re Message
- * @author    Oleg Kozlov <h1karo@remessage.ru>
- * @license   Apache License 2.0
- * @license   https://legal.remessage.ru/licenses/message
+ * (c) 2018-present Re Message
+ *     Oleg Kozlov <h1karo@outlook.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ *
+ * https://dev.remessage.ru/packages/message
+ * https://github.com/re-message/message
  */
 
 namespace RM\Standard\Message;
